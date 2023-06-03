d55026d24bfd0bfd101623896062d11336418238
molly todd <mollyjt89@gmail.com>
2023-06-02 18:13:13 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "<h1>Molly is the champion</h1>";
+
+newHeader.id= "victory";
